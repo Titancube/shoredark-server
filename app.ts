43e76f0224c098409389af00beb816848552e3a5
Titancube <kbin0717@gmail.com>
2022-05-02 11:01:14 +0900

    chore: commentate

--- app.ts
+++ app.ts
@@ -1,32 +1,11 @@
 import app from "./plugins/express";
-import db from "./plugins/firebase";
-import fn from "./plugins/functions";
 
-// Admin check
-app.get("/auth", async (req, res) => {
-  try {
-    const uid = req.query.uid + "";
-    const dbReq = await db.collection("Admins").doc(uid).get();
-    if (dbReq.exists) {
-      res.status(200).json({
-        isAdmin: true,
-      });
-    } else {
-      res.status(404).json({
-        isAdmin: false,
-      });
-    }
-  } catch (error) {
-    fn.logger(`Server error`);
-    console.error(error);
-  }
-});
-
+// Test connection
 app.get("/api", (req, res) => {
   res.status(200).send("done man");
 });
 
-// Listen on port 8000
+// Listen on port 5000
 app.listen(5000, () => {
   console.log(`[${new Date()}] Shoredark API server is up`);
 });
